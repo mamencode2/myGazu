6081c92e852055387685d7bd0eae21847fd43f9c
mamencode2 <mamencode2@gmail.com>
2022-05-21 02:00:59 +0000

    cd

---
+++ gazuDev/assets/data/fuelData.js
@@ -0,0 +1,21 @@
+const fuelData = [
+    {
+    id: '1',
+    name: 'Gasolin',
+    fuel: 'benzin',
+    image: require('../images/Sc.webp'),
+    checked: false
+    },
+    {
+    id: '2',
+    name: 'Disel',
+    fuel: 'Nafta',
+    image: require('../images/Pt.webp'),
+    checked: false
+    }
+    ]
+    
+    
+    
+    
+    export default fuelData
---
+++ gazuDev/assets/data/stationData.js
@@ -0,0 +1,330 @@
+const stationData= [
+    {
+       id: 1,
+       
+       geometry:{
+        type: "Point",
+        coordinates: [
+          38.842625,
+          9.021844
+        ]
+       },
+       properties:{
+           stName: 'Nile Petrolium',
+           phoneNumber: '',
+           address: '',
+           city: '',
+           postalCode: '', 
+           email: '',
+           web: ''
+       },
+       workingHours:{
+           open: '',
+           close: ''
+       }, 
+       
+       stockStatus: {
+           benzin: '370000',
+           Nafta: '18673'
+       }
+    },
+    {
+        id: 2,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.790344,
+           8.990382
+         ]
+        },
+        properties:{
+            stName: 'Oilibya',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '0',
+            Nafta: '23456'
+        }
+     },
+     {
+        id: 3,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.793621,
+           8.991411
+         ]
+        },
+        properties:{
+            stName: 'Total Bras',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '98765',
+            Nafta: '1234'
+        }
+     },
+     {
+        id: 4,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.804035,
+           9.001441
+         ]
+        },
+        properties:{
+            stName: 'Noc gerji',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '19000',
+            Nafta: '0'
+        }
+     },
+     {
+        id: 5,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.794122,
+           9.006546
+         ]
+        },
+        properties:{
+            stName: 'Total Papaya',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '0',
+            Nafta: '15000'
+        }
+     },
+     {
+        id: 6,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.804097,
+           9.012000
+         ]
+        },
+        properties:{
+            stName: 'Gomju',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '34000',
+            Nafta: '0'
+        }
+     },
+     {
+        id: 7,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.812097,
+           9.021397
+         ]
+        },
+        properties:{
+            stName: 'Total Lamberet',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '29000',
+            Nafta: '18997'
+        }
+     },
+     {
+        id: 8,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.811471,
+           9.021874
+         ]
+        },
+        properties:{
+            stName: 'NOC Lamberet',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '12000',
+            Nafta: '3245'
+        }
+     },
+     {
+        id: 9,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.796781,
+           9.017989
+         ]
+        },
+        properties:{
+            stName: 'Total Lemhotel',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '4355',
+            Nafta: '17654'
+        }
+     },
+     {
+        id: 10,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.794264,
+           9.017961
+         ]
+        },
+        properties:{
+            stName: 'NOC Lemhotel',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '41000',
+            Nafta: '0'
+        }
+     },
+     {
+        id: 11,
+        
+        geometry:{
+         type: "Point",
+         coordinates: [
+           38.836153,
+           9.023499
+         ]
+        },
+        properties:{
+            stName: 'NOC CMC',
+            phoneNumber: '',
+            address: '',
+            city: '',
+            postalCode: '', 
+            email: '',
+            web: ''
+        },
+        workingHours:{
+            open: '',
+            close: ''
+        }, 
+        
+        stockStatus: {
+            benzin: '0',
+            Nafta: '0'
+        }
+     }
+]
+
+
+
+
+export default stationData
+
+
+
+//npm install @mapbox/mapbox-sdk
